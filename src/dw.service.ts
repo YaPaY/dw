be5e14a8ffd31024741c65c76a526de6f71ec936
Alex0007 <Alex0007lolpvp@gmail.com>
2021-08-21 04:49:35 +0500

    updates

--- src/dw.service.ts
+++ src/dw.service.ts
@@ -1,64 +1,49 @@
-import qs from "querystring";
-import { FetchFn } from "@mediaurl/sdk/src/tasks";
-import fetch from "node-fetch";
+import { MovieItem, PlayableItem, VODItem } from "@mediaurl/sdk/dist";
+import cheerio from "cheerio";
 
-export interface InformationSpace {
-  id: number;
-  news: NewsItem[];
-}
+export const parseList = (html: string) => {
+  const $ = cheerio.load(html);
 
-export interface NewsItem {}
+  const result: VODItem[] = [];
 
-export interface NewsContent {
-  id: number;
-  name: string;
-  shortTitle: string;
-  contentDate: string;
-  __typename: "Video" | "Article";
-}
+  const elements = [
+    // Top stories page
+    ...$("div.col2.basicTeaser").toArray(),
 
-export interface InformationSpace {}
+    // Search page
+    ...$("#searchResults div.news.searchres").toArray(),
+  ];
 
-export const executeQuery = <T>(
-  opName: string,
-  payload: any,
-  fetcher: FetchFn
-): Promise<T> => {
-  const url =
-    "https://beta.dw.com/graphql?" +
-    `&operationName=${opName}` +
-    `&variables=${payload}` +
-    `&extensions={\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"0318491f2c7acd82aa9c09fe24967508d94dae86c9baebc5377e9161dab8e76b\"}}`;
+  elements.forEach((elem) => {
+    result.push({
+      type: "movie",
+      name: $(elem).find("h2").contents().first().text().trim(),
+      description: $(elem).find("p").first().text(),
+      ids: { id: $(elem).find("a").attr("href") as string },
+      images: { poster: $(elem).find("img").attr("src") },
+    });
+  });
 
-  console.log({ url });
-
-  return fetcher(url, {
-    headers: {
-      "content-type": "application/json",
-    },
-  }).then((resp) => resp.json());
+  return result;
 };
 
-export const extractItems = (
-  input: any,
-  predicate: (item) => boolean,
-  acc: any[] = []
-) => {
-  if (!input) return acc;
+export const extractVideoIds = (
+  html: string
+): { id: string; name: string }[] => {
+  return [];
+};
 
-  if (predicate(input)) {
-    acc.push(input);
-  }
+export const parseStreamSources = (html: string): MovieItem["sources"] => {
+  const $ = cheerio.load(html);
 
-  if (Array.isArray(input)) {
-    input.forEach((x) => extractItems(x, predicate, acc));
-  }
-
-  if (typeof input === "object" && input !== null) {
-    Object.values(input).forEach((item) => {
-      return extractItems(item, predicate, acc);
+  return $("div.mediaItem")
+    .toArray()
+    .map((elem) => {
+      return {
+        name: $(elem)
+          .find('input[name="channel_name"]')
+          .attr("value") as string,
+        url: $(elem).find('input[name="file_name"]').attr("value") as string,
+      };
     });
-  }
-
-  return acc;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import * as fs from "fs";
-import { createAddon, MovieItem, runCli } from "@mediaurl/sdk";
-import { executeQuery, extractItems, NewsContent } from "./dw.service";
+import {
+  createAddon,
+  ItemResponse,
+  MovieItem,
+  PlayableItem,
+  runCli,
+} from "@mediaurl/sdk";
+import { extractVideoIds, parseList, parseStreamSources } from "./dw.service";
 
 const dwAddon = createAddon({
   id: "dw",
@@ -12,7 +18,11 @@ const dwAddon = createAddon({
   catalogs: [
     {
       name: "Top stories",
-      id: 9097,
+      id: "s-9097",
+    },
+    {
+      name: "Media content",
+      id: "s-100826",
     },
   ],
 });
@@ -20,49 +30,87 @@ const dwAddon = createAddon({
 dwAddon.registerActionHandler("catalog", async (input, ctx) => {
   console.log("catalog", input);
 
-  const resp = await executeQuery(
-    "getNavigationPage",
-    JSON.stringify({
-      id: input.catalogId,
-      lang: "ENGLISH",
-    }),
-    ctx.fetch
-  );
+  const html = await ctx
+    .fetch(`https://www.dw.com/en/${input.catalogId}`)
+    .then((_) => _.text());
 
-  fs.writeFileSync("dump.json", JSON.stringify(resp));
-
-  const extracted = extractItems(
-    resp,
-    (_) => ["Video", "Article"].indexOf(_.__typename) !== -1
-  );
+  const items = parseList(html);
 
   return {
     options: { displayName: true, imageShape: "landscape" },
-    items: extracted
-      .filter((_) => {
-        return _.posterImageLink || _.mainContentImageLink;
-      })
-      .map((_) => {
-        return {
-          type: "movie",
-          ids: { id: _.id },
-          name: _.shortTitle,
-          images: {
-            poster:
-              _.posterImageLink?.stillUrl ||
-              _.mainContentImageLink?.target.staticUrl.replace(
-                "${formatId}",
-                "604"
-              ),
-          },
-        };
-      }),
+    items: [
+      (input.catalogId === "s-9097"
+        ? {
+            type: "movie",
+            name: "Live TV",
+            ids: { id: "/en/s-100825" },
+            images: { poster: "https://static.dw.com/image/47222913_304.png" },
+          }
+        : null) as MovieItem,
+      ...items,
+    ].filter((_) => _),
     nextCursor: null,
   };
 });
 
 dwAddon.registerActionHandler("item", async (input, ctx) => {
   console.log("item", input);
+
+  const common = {
+    type: "movie",
+    ids: input.ids,
+  };
+  const isStream = /s-(\d*)$/.test(input.ids.id as string);
+  const isVideo = /v-(\d*)$/.test(input.ids.id as string);
+  const videoIdMatch = /-(\d*)$/.exec(input.ids.id as string);
+
+  if (!videoIdMatch) {
+    throw new Error("Unable to read ID from link");
+  }
+
+  if (isStream) {
+    const html = await ctx
+      .fetch(`https://www.dw.com/en/media-center/live-tv/s-100825?hls=true`)
+      .then((_) => _.text());
+    return { ...common, sources: await parseStreamSources(html) };
+  }
+
+  const getSource = (id: string) => {
+    return ctx
+      .fetch(`https://www.dw.com/playersources/v-${videoIdMatch[1]}?hls=true`, {
+        headers: {
+          "content-type": "application/json",
+        },
+      })
+      .then<{ file: string }[]>((_) => _.json());
+  };
+
+  const sources: PlayableItem["sources"] = await (isVideo
+    ? getSource(videoIdMatch[1]).then((_) => {
+        return _.map(({ file }) => {
+          return { url: file };
+        });
+      })
+    : ctx
+        .fetch(
+          `https://www.dw.com/en/merkel-asks-russia-to-pressure-taliban-on-evacuations/a-58915545`
+        )
+        .then((_) => _.text())
+        .then(extractVideoIds)
+        .then((results) =>
+          Promise.all(
+            results.map(({ id, name }) => {
+              return getSource(id)
+                .then((_) => _[0].file)
+                .then((url) => ({ url, name }));
+            })
+          )
+        ));
+
+  return {
+    ...common,
+    sources,
+  };
 });
 
 runCli([dwAddon]);
